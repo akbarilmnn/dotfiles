9c726d46e14afb30e54b52f42ad1e159dde35207
akbar <akbarsmp27@gmail.com>
2023-09-28 11:49:22 +0700

    initialize bufferline.

--- .config/nvim/lua/custom/plugins/bufferline.lua
+++ .config/nvim/lua/custom/plugins/bufferline.lua
@@ -1,7 +1,8 @@
 return {
-    "akinsho/bufferline.nvim",
-    version = "*",
-    dependencies = {
-        "nvim-tree/nvim-web-devicons",
-    },
+	"akinsho/bufferline.nvim",
+	version = "*",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	opts = {},
 }
