aa950ee501abd3424a4adc9e41f8b32ed4386f82
akbar <akbarsmp27@gmail.com>
2024-03-13 14:50:07 +0700

    remove previous colorscheme, replaced it with new one.

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,37 +1,4 @@
 return {
-	-- thanks to (https://github.com/dmmulroy/kickstart.nix)
-	"catppuccin/nvim",
-	config = function()
-		require("catppuccin").setup({
-			integrations = {
-				cmp = true,
-				gitsigns = true,
-				harpoon = true,
-				illuminate = true,
-				indent_blankline = {
-					enabled = false,
-					scope_color = "sapphire",
-					colored_indent_levels = false,
-				},
-				mason = true,
-				native_lsp = { enabled = true },
-				notify = true,
-				nvimtree = true,
-				neotree = true,
-				symbols_outline = true,
-				telescope = true,
-				treesitter = true,
-				treesitter_context = true,
-			},
-			-- TODO: override some highlights such that it matches for vscode dark plust theme
-			-- :help highlight to see the current highlighted color for each highlighted item.
-		})
-
-		vim.cmd.colorscheme("catppuccin-mocha")
-
-		-- Hide all semantic highlights until upstream issues are resolved (https://github.com/catppuccin/nvim/issues/480)
-		for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
-			vim.api.nvim_set_hl(0, group, {})
-		end
-	end,
+	"LunarVim/darkplus.nvim",
+	opts = {},
 }
