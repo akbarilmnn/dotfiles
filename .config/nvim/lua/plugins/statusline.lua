b4e856ec0c8594e3ce4896875bb81c31af54c99d
akbar <akbarsmp27@gmail.com>
2024-03-13 14:51:35 +0700

    experimenting with lazy.nvim package manager and changed theme to auto n statusline.lua .

--- .config/nvim/lua/plugins/statusline.lua
+++ .config/nvim/lua/plugins/statusline.lua
@@ -20,7 +20,7 @@ return {
 
 		require("lualine").setup({
 			options = {
-				theme = "catppuccin",
+				theme = "auto",
 				globalstatus = true,
 				component_separators = { left = "", right = "" },
 				section_separators = { left = "█", right = "█" },
